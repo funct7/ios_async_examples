151293e2ef26c505d7865e462d756a135a927936
Joshua Park <wmpark@knowre.com>
2020-08-31 13:58:17 +0900

    Add: Types for mocking

---
+++ ConcurrencyExamples/Extension/URL+Constant.swift
@@ -0,0 +1,16 @@
+//
+//  URL+Constant.swift
+//  ConcurrencyExamples
+//
+//  Created by Joshua Park on 2020/08/31.
+//  Copyright © 2020 Knowre. All rights reserved.
+//
+
+import Foundation
+
+extension URL {
+    
+    static let userImageURL: URL = URL(string: "/foo")!
+    static let otherUserImageURL: URL = URL(string: "/bar")!
+    
+}
---
+++ ConcurrencyExamples/Type/TestError.swift
@@ -0,0 +1,13 @@
+//
+//  TestError.swift
+//  ConcurrencyExamples
+//
+//  Created by Joshua Park on 2020/08/31.
+//  Copyright © 2020 Knowre. All rights reserved.
+//
+
+import Foundation
+
+enum TestError : Error {
+    case expected
+}
