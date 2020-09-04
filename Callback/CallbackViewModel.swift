8f58dcc9e7b3b15ca4688658e84ca01fd0c20f68
Joshua Park <wmpark@knowre.com>
2020-09-04 13:18:06 +0900

    Fix: Callback - ViewModel network indicator logic

--- Callback/CallbackViewModel.swift
+++ Callback/CallbackViewModel.swift
@@ -44,59 +44,85 @@ final class CallbackViewModel : ObservableObject {
     }
     
     func signIn() {
-        isLoading = true
+        incrementLoading()
         
         UserRepository.shared.signIn { [weak self] (user, error) in
+            guard let self = self else { return }
+            
             if let user = user {
-                self?.user = User(id: user.id)
-                self?.user?.username = user.username
+                self.user = User(id: user.id)
+                self.user?.username = user.username
+                
+                self.incrementLoading()
                 
                 ImageRepository.shared.resolveImage(url: user.pictureURL) { [weak self] (image, error) in
+                    guard let self = self else { return }
+                    
                     if let image = image {
-                        self?.user?.userImage = image
+                        self.user?.userImage = image
                     } else {
-                        self?.setAlertMessage(error!.localizedDescription)
+                        self.setAlertMessage(error!.localizedDescription)
                     }
+                    
+                    self.decrementLoading()
                 }
                 
+                self.incrementLoading()
+                
                 PostRepository.shared.fetchPost(userID: user.id) { [weak self] (postList, error) in
+                    guard let self = self else { return }
+                    
                     if let postList = postList {
-                        self?.feed = postList.map {
+                        self.feed = postList.map {
                             Post(id: $0.id, userID: $0.userID, content: $0.content)
                         }
                         
                         postList
                             .reduce(into: Set<String>()) { $0.insert($1.userID) }
                             .forEach {
+                                self.incrementLoading()
                                 UserRepository.shared.fetchUser(id: $0) { [weak self] (user, error) in
+                                    guard let self = self else { return }
+                                    
                                     if let user = user {
-                                        self?.feed
+                                        self.feed
                                             .filter { $0.userID == user.id }
                                             .forEach { $0.username = user.username }
                                         
-                                        ImageRepository.shared.resolveImage(url: user.pictureURL) { (image, error) in
+                                        self.incrementLoading()
+                                        ImageRepository.shared.resolveImage(url: user.pictureURL) { [weak self] (image, error) in
+                                            guard let self = self else { return }
+                                            
                                             if let image = image {
-                                                self?.feed
+                                                self.feed
                                                     .filter { $0.userID == user.id }
                                                     .forEach { $0.userImage = image }
-                                                
-                                                self?.isLoading = false
                                             } else {
-                                                self?.setAlertMessage(error!.localizedDescription)
+                                                self.setAlertMessage(error!.localizedDescription)
                                             }
+                                            
+                                            self.decrementLoading()
                                         }
                                     } else {
-                                        self?.setAlertMessage(error!.localizedDescription)
+                                        self.setAlertMessage(error!.localizedDescription)
                                     }
+                                    
+                                    self.decrementLoading()
                                 }
                             }
                     } else {
-                        self?.setAlertMessage(error!.localizedDescription)
+                        self.setAlertMessage(error!.localizedDescription)
                     }
+                    
+                    self.decrementLoading()
                 }
+                
+                self.decrementLoading()
             } else {
-                self?.setAlertMessage(error!.localizedDescription)
+                self.setAlertMessage(error!.localizedDescription)
             }
+            
+            self.decrementLoading()
         }
     }
     
@@ -105,6 +131,18 @@ final class CallbackViewModel : ObservableObject {
     private func setAlertMessage(_ message: String) {
         alertMessage = message
         DispatchQueue.main.async { self.alertMessage = nil }
+    }
+    
+    private var loadingCount: Int = 0 {
+        didSet { isLoading = loadingCount > 0 }
+    }
+    
+    private func incrementLoading() {
+        loadingCount += 1
+    }
+    
+    private func decrementLoading() {
+        loadingCount -= 1
     }
     
 }
